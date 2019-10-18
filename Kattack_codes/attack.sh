3aeb7cbfa7657cfd9b65b6960351d502f781a4ed
Yuxin Cui <yuxinc@node0104.palmetto.clemson.edu>
2019-10-18 09:12:13 -0400

    attack xgb diabets

--- Kattack_codes/attack.sh
+++ Kattack_codes/attack.sh
@@ -5,7 +5,7 @@ nohup python xgbKantchelianAttack.py -d=../chosen_sample/xgb/breast_cancer_xgb_s
 #covtype todo
 nohup python xgbKantchelianAttack.py -d=../chosen_sample/xgb/covtype_xgb_samples.svm -m=../models/xgb/covtype_xgb.model -c=7 >../Kattack_result/xgb/covtype_xgb_log.txt 2>&1 &
 #diabetes todo
-nohup python xgbKantchelianAttack.py -d=../chosen_sample/xgb/diabetes_xgb_samples.svm -m=../models/xgb/diabetes_xgb.model -c=2 >../Kattack_result/xgb/diabetes_xgb_log.txt 2>&1 &
+nohup python xgbKantchelianAttack.py -d=../chosen_sample/xgb/diabetes_xgb_samples.pkl -m=../models/xgb/diabetes_xgb.model -c=2 >../Kattack_result/xgb/diabetes_xgb_log.txt 2>&1 &
 #MNIST mulclass
 nohup python xgbKantchelianAttack.py -d=../chosen_sample/xgb/MNIST2_6_xgb_samples.pkl -m=../models/xgb/MNIST2_6_xgb.model -c=2 >../Kattack_result/xgb/MNIST2_6_xgb_log.txt 2>&1 &
 # attack robust xgb model
@@ -15,4 +15,4 @@ nohup python xgbKantchelianAttack.py -d=../chosen_sample/rxgb/cod_rna_rxgb_sampl
 #covtype todo 5000
 nohup python xgbKantchelianAttack.py -d=../chosen_sample/rxgb/covtype_rxgb_samples.s -m=../models/rxgb/covtype/covtype_rxgb.model -c=7 >../Kattack_result/rxgb/covtype_rxgb_log.txt 2>&1 &
 nohup python xgbKantchelianAttack.py -d=../chosen_sample/rxgb/diabetes_rxgb_samples.s -m=../models/rxgb/diabetes/diabetes_rxgb.model -c=2 >../Kattack_result/rxgb/diabetes_rxgb_log.txt 2>&1 &
-nohup python xgbKantchelianAttack.py -d=../chosen_sample/rxgb/MNIST_rxgb_samples.s -m=../models/rxgb/MNIST_rxgb.model -c=10 >../Kattack_result/rxgb/MNIST_rxgb_log.txt 2>&1 &
+nohup python xgbKantchelianAttack.py -d=../chosen_sample/rxgb/MNIST2_6_rxgb_samples.s -m=../models/rxgb/MNIST2_6_rxgb.model -c=2 >../Kattack_result/rxgb/MNIST2_6_rxgb_log.txt 2>&1 &
