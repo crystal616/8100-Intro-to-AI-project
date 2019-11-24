298c2fbf1cf8c11b774f8e5aa4d789b1ebc5cf2f
Ying Cai <cai7@g.clemson.edu>
2019-11-24 11:24:26 -0500

    for robust xgb models

--- codes/report 3/cheng_attack_rxgb_driver.py
+++ codes/report 3/cheng_attack_rxgb_driver.py
@@ -1,36 +1,61 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 14 18:14:27 2019
+Created on Wed Oct 16 18:14:27 2019
 
 @author: Ying
 """
 
-import argparse
-
 import pandas as pd
 import xgboost as xgb
-import numpy as np
 import os
 curr = os.getcwd()
 find = curr.find('cai_cui')
 home = curr[:find+7]
 os.chdir('{}/codes'.format(home))
-import cheng_attack
+import cheng_attack_rxgb
 from sklearn.datasets import load_svmlight_file
-import scipy
+from scipy import sparse
 import time
+import argparse
+import numpy as np
 
-def predict(model, sampleX, nclasses):
-    dtest = xgb.DMatrix(scipy.sparse.csr_matrix(sampleX))
-    prediction = model.predict(dtest)
-    if nclasses == 2:
-        if prediction<0.5:
-            prediction = 0
-        else:
-            prediction = 1
-    else:
-        prediction = int(prediction)
-    return prediction
+class xgboost_wrapper():
+	def __init__(self, model, binary=False):
+		self.model = model 
+		self.binary = binary
+		#print('binary classification: ',self.binary)
+	def maybe_flat(self, input_data):
+		if not isinstance(input_data,np.ndarray):
+			print(type(input_data))
+			input_data = np.copy(input_data.numpy())
+		shape = input_data.shape
+		if len(input_data.shape) == 1:
+			input_data = np.copy(input_data[np.newaxis,:])
+		if len(input_data.shape) >= 3:
+			input_data = np.copy(input_data.reshape(shape[0],np.prod(shape[1:])))
+		return input_data, shape
+	def predict(self, input_data):
+		input_data, _ = self.maybe_flat(input_data)
+		ori_input = np.copy(input_data)
+		np.clip(input_data, 0, 1, input_data)
+		input_data = xgb.DMatrix(sparse.csr_matrix(input_data)) 
+		ori_input = xgb.DMatrix(sparse.csr_matrix(ori_input))
+		test_predict = np.array(self.model.predict(input_data))
+		if self.binary:
+			test_predict = (test_predict > 0.5).astype(int)
+		else:
+			test_predict = test_predict.astype(int) 
+		return test_predict[0]
+	def predict_logits(self, input_data):
+		input_data, _ = self.maybe_flat(input_data) 
+		#input_back = np.copy(input_data)
+		input_data = sparse.csr_matrix(input_data) 
+		input_data = xgb.DMatrix(input_data) 
+		test_predict = np.array(self.model.predict(input_data))
+		return test_predict
+	def predict_label(self, input_data):
+		return self.predict(input_data)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -48,13 +73,13 @@ if __name__ == '__main__':
     nclasses = args.c
     nfeatures = args.n
     f_start = args.feature_start
-
-    model = xgb.Booster()
-    model.load_model(model_path)
     
+    bst = xgb.Booster()
+    bst.load_model(model_path)
     test_data, test_label = load_svmlight_file(data_path, n_features = nfeatures)
     test_data = test_data.toarray()
     test_label = test_label.astype('int')
+
     n = len(test_label)
     df = pd.DataFrame(test_data)
     df['label'] = test_label
@@ -62,27 +87,31 @@ if __name__ == '__main__':
     test_label = df['label'].tolist()
     test_data = np.array(df.drop(columns=['label']))
     
-    if n<200:
+    if n<10:
         n_selected = n
     else:
-        n_selected = 200
+        n_selected = 10
+
+    if nclasses == 2:
+        binary = True
+    else:
+        binary = False
         
-        
+    m = xgboost_wrapper(bst, binary)
     ori_points = []
     results = []
     used_time = []
     for tt in range(n_selected):
         s = test_data[tt]
         sl = test_label[tt]
-        if predict(model, s, nclasses) == sl:        
+        if m.predict(s) == sl:        
             start = time.time()
-            r = cheng_attack.attack(model, test_data, test_label, s, sl, nclasses, tt)
+            r = cheng_attack_rxgb.attack(bst, test_data, test_label, s, sl, nclasses, tt)
             end = time.time()
             used_time.append(end - start)
             ori_points.append(s)
             results.append(r)
             print('sample {} is done'.format(tt))
-        
 
     total_dis = 0
     pert = pd.DataFrame()
@@ -95,17 +124,17 @@ if __name__ == '__main__':
         dis.append(d)
         total_dis += d
 
-
     pert['index'] = index
     pert['distance'] = dis
     pert['used_time'] = used_time
     pert['pert point'] = points
     pert['ori point'] = ori_points
     os.chdir('{}/attack/cheng'.format(home))
-    pert.to_csv('{}_cheng_attack_xgb.txt'.format(d_name))
-    with open('{}_cheng_xgb_ave.txt'.format(d_name), 'w') as f:
+    pert.to_csv('{}_cheng_attack_rxgb.txt'.format(d_name))
+    with open('{}_cheng_rxgb_ave.txt'.format(d_name), 'w') as f:
         f.write('average distance: ' + str(total_dis/len(test_label)))
         f.write('\n\naverage used time: ' + str(np.mean(pert['used_time'])))
 
     f.close()
     print('{} is done'.format(d_name))
+    
